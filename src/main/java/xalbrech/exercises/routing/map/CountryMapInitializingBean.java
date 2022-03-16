73379f6442cec9f49de790440af878f999ffdae2
david <d.albrecht@seznam.cz>
2022-03-16 11:48:33 +0100

    minor: logging

--- src/main/java/xalbrech/exercises/routing/map/CountryMapInitializingBean.java
+++ src/main/java/xalbrech/exercises/routing/map/CountryMapInitializingBean.java
@@ -1,10 +1,13 @@
 package xalbrech.exercises.routing.map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import xalbrech.exercises.routing.calculation.RouteCalculator;
 
 /**
  * Initializing bean - populates the country map.
@@ -12,11 +15,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class CountryMapInitializingBean implements InitializingBean {
 
+    private static final Logger log = LoggerFactory.getLogger(CountryMapInitializingBean.class);
+
     @Autowired
-    CountryMap countryMap;
+    private CountryMap countryMap;
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        log.info("Populating map of countries...");
         countryMap.initMap();
+        log.info("Map of countries populated.");
     }
 }
